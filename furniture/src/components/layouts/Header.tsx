96aada22f61a2b550dbc35179128917245b09658
Nang Pyae Pyae Phyoe Aein <pyaephyoeaein@gmail.com>
2026-04-21 18:00:48 +0700

    mobile nav

--- furniture/src/components/layouts/Header.tsx
+++ furniture/src/components/layouts/Header.tsx
@@ -1,6 +1,7 @@
 import MainNavigation from "./MainNavigation";
 import MobileNavigation from "./MobileNavigation";
 import { siteConfig } from "@/config/site";
+import { ModeToggle } from "../mode-toggle";
 
 function Header() {
     return (
@@ -8,6 +9,9 @@ function Header() {
             <nav className="container flex items-center h-16 mx-auto">
                 <MainNavigation items={siteConfig.mainNav} />
                 <MobileNavigation items={siteConfig.mainNav} />
+                <div className="flex flex-1 items-center justify-end space-x-4 mr-8 lg:mr-0">
+                    <ModeToggle />
+                </div>
             </nav>
         </header>
     )
--- furniture/src/components/layouts/MobileNavigation.tsx
+++ furniture/src/components/layouts/MobileNavigation.tsx
@@ -14,6 +14,12 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet"
 import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 interface MainNavigationProps {
     items?: MainNavItem[];
@@ -28,8 +34,44 @@ export default function MobileNavigation({ items }: MainNavigationProps) {
                         <span className="sr-only">Toggle Menu</span>
                     </Button>
                 </SheetTrigger>
-                <SheetContent side="left" className="pl-1 pr-0 pt-9">
-
+                <SheetContent side="left" className="pt-9">
+                    <SheetClose asChild>
+                        <Link to="/" className="flex items-center space-x-2">
+                            <Icons.logo className="size-4 mr-2" />
+                            <span className="font-bold">{siteConfig.name}</span>
+                            <span className="sr-only">Home</span>
+                        </Link>
+                    </SheetClose>
+                    <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-8">
+                        <Accordion
+                            type="multiple"
+                            className="w-full"
+                        >
+                            <AccordionItem value="item-1">
+                                <AccordionTrigger>{items?.[0].title}</AccordionTrigger>
+                                <AccordionContent>
+                                    <div className="flex flex-col space-y-2 pl-2">
+                                        {items?.[0].card?.map((item) => (
+                                            <SheetClose key={item.href} asChild>
+                                                <Link to={item.href} className="text-foreground/70">
+                                                    {item.title}
+                                                </Link>
+                                            </SheetClose>
+                                        ))}
+                                    </div>
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
+                        <div className="flex flex-col space-y-2 mt-4">
+                            {items?.[0].menu?.map((item) => (
+                                <SheetClose key={item.href} asChild>
+                                    <Link to={item.href}>
+                                        {item.title}
+                                    </Link>
+                                </SheetClose>
+                            ))}
+                        </div>
+                    </ScrollArea>
                 </SheetContent>
             </Sheet>
         </div>
