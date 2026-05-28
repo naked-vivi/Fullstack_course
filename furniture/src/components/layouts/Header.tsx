e160a966d2ca12638776ecfde29574905bd3827f
Nang Pyae Pyae Phyoe Aein <pyaephyoeaein@gmail.com>
2026-05-28 18:29:05 +0700

    Add:Cart

--- furniture/src/components/layouts/Header.tsx
+++ furniture/src/components/layouts/Header.tsx
@@ -4,6 +4,7 @@ import { siteConfig } from "@/config/site";
 import { ModeToggle } from "../mode-toggle";
 import AuthDropDown from "./AuthDropDown";
 import { User } from "@/data/user";
+import CartSheet from "./CartSheet";
 
 function Header() {
     return (
@@ -12,6 +13,7 @@ function Header() {
                 <MainNavigation items={siteConfig.mainNav} />
                 <MobileNavigation items={siteConfig.mainNav} />
                 <div className="flex flex-1 items-center justify-end space-x-4 mr-8 lg:mr-0">
+                    <CartSheet />
                     <ModeToggle />
                     <AuthDropDown user={User} />
                 </div>
